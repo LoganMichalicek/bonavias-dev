cccdd956e9c17b1f4fb27edca711de81f0fa04c4
Logan Michalicek <logan.michalicek@gmail.com>
2022-03-28 08:42:39 -0600

    Added react icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookSquare, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center h-12 bg-black text-white'>
-      <p>Copyright Bonavias (c) All Rights Reserved</p>
+    <div className="h-12 bg-black text-white flex justify-between items-center">
+      <div className="flex">
+        <p className="pl-3">© 2020 The Bona Vias Foundation</p>
+        <Link to="/privacy" className="pl-3">
+          <span>Privacy Policy</span>
+        </Link>
+        <Link to="/terms" className="pl-3">
+          <span>Terms of Service</span>
+        </Link>
+      </div>
+      <div className="flex">
+        <p className="pr-3">Follow Us!</p>
+        <FaFacebookSquare className="pr-3 h-7 w-7" />
+        <FaTwitter className="pr-3 h-7 w-7" />
+        <FaInstagram className="pr-3 h-7 w-7" />
+        <FaYoutube className="pr-3 h-7 w-7" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
